097f9210911e406b3a7aba707f8c8330f4b01867
William J <william.jelvin@gmail.com>
2020-08-06 13:54:00 +0200

    Switched to swedish

--- Dictionary.Data/Models/Translation.cs
+++ Dictionary.Data/Models/Translation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,25 @@ namespace Dictionary.Data.Models
     public class Translation
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Svensk titel")]
         public string SwedishTitle { get; set; }
+        [Required]
+        [Display(Name = "Engelsk titel")]
         public string EnglishTitle { get; set; }
+        [DisplayFormat(NullDisplayText = "Inte angiven")]
+        [Display(Name = "Svensk ingress")]
         public string SwedishPreamble { get; set; }
+        [DisplayFormat(NullDisplayText = "Inte angiven")]
+        [Display(Name = "Engelsk ingress")]
         public string EnglishPreamble { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Svensk huvudtext")]
         public string SwedishText { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Engelsk huvudtext")]
         public string EnglishText { get; set; }
     }
 }
--- Dictionary.Web/App_Start/RouteConfig.cs
+++ Dictionary.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Dictionary.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Translations", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
